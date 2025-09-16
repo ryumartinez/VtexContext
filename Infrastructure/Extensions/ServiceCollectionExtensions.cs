86d1a88a9764336eb89849782a211832a4ec2cd1
Ryu Martinez <ryueduardomartinez@outlook.es>
2025-09-15 21:57:03 -0300

    added Infrastructure.openapi_specClient

--- Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -10,6 +10,15 @@ public static class ServiceInjection
     public static void ConfigureServices(IServiceCollection services)
     { 
         services.AddHttpClient();
+        services.AddHttpClient<Infrastructure.openapi_specClient>(client =>
+        {
+            // Set the base address from your configuration
+            client.BaseAddress = new Uri("BaseUrl");
+
+            // Add the authentication headers that are required for EVERY request
+            client.DefaultRequestHeaders.Add("X-VTEX-API-AppKey", "AppKey");
+            client.DefaultRequestHeaders.Add("X-VTEX-API-AppToken", "AppToken");
+        });
         services.AddKeyedScoped<IProductRepository, VtexProductRepository>("Vtex");
         services.AddKeyedScoped<IProductRepository, SqlProductRepository>("Sql");
         services.AddIdentity<ApplicationUser, IdentityRole>()
